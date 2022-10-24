309fb5ac10f421ada7ecbd27b91a150611e823a9
H Tendy <tendy@hotmail.com>
2022-10-24 14:28:29 +0530

    Quieten the logs now

--- acl.js
+++ acl.js
@@ -5,23 +5,19 @@ async function aclCheck (dsName, dsView, dsUser) {
         let dbAbstraction = new DbAbstraction();
         let aclConfig = await dbAbstraction.find(dsName, "metaData", { _id: `aclConfig` }, {} );
         aclConfig = aclConfig[0];
-        console.log("In aclCheck, aclConfig: ", aclConfig);
         if (!aclConfig) {
             return true
         }
         if (!aclConfig.accessCtrl) {
-            console.log("Returning true");
             return true
         }
         console.log("User is: ", dsUser);
         if (aclConfig.acl.includes(dsUser)) {
-            console.log("Returning true 2");
             return true
         }
     } catch (e) {
         console.log("In aclCheck, exception: ", e);
     }
-    console.log("Returning false")
     return false;
 }
 
