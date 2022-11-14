d3d3f9170e69c84e9fed482b88a0d0fcd2e0c1d7
H Tendy <tendy@hotmail.com>
2022-11-14 08:34:49 +0530

    Fix leak

--- acl.js
+++ acl.js
@@ -6,18 +6,22 @@ async function aclCheck (dsName, dsView, dsUser) {
         let aclConfig = await dbAbstraction.find(dsName, "metaData", { _id: `aclConfig` }, {} );
         aclConfig = aclConfig[0];
         if (!aclConfig) {
+            await dbAbstraction.destroy();            
             return true
         }
         if (!aclConfig.accessCtrl) {
+            await dbAbstraction.destroy();
             return true
         }
         console.log("User is: ", dsUser);
         if (aclConfig.acl.includes(dsUser)) {
+            await dbAbstraction.destroy();
             return true
         }
     } catch (e) {
         console.log("In aclCheck, exception: ", e);
     }
+    await dbAbstraction.destroy();
     return false;
 }
 
