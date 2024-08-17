4715e8fe43cc8e59679243cb52e9601e22da3dda
Hrishikesh Kumar <hkumar@infinera.com>
2024-08-17 11:20:22 +0530

    Increased payload size limit

--- server.js
+++ server.js
@@ -72,9 +72,11 @@ try {
 }
 httpServer.timeout = 60 * 60 * 1000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({'limit': '200mb', extended: true }));
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    'limit': '200mb'
+}));
 app.use(compression());
 app.use(express.static(reactuiDir));
 
